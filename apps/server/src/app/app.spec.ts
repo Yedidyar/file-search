eb8e32839ab148f0d83abb40ecc0acd8add24fe0
Nikita <nikitabecker2003@gmail.com>
2025-06-19 18:37:44 +0300

    add backend

---
+++ apps/server/src/app/app.spec.ts
@@ -0,0 +1,7 @@
+import { describe, it, expect } from 'vitest';
+
+describe('App Module', () => {
+    it('should return true for a basic truthy test', () => {
+        expect(true).toBe(true);
+    });
+});
