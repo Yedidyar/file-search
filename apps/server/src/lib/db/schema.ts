38ddda1102ceb7d7de509620ba1f4782e84a5e6f
Nikita <nikitabecker2003@gmail.com>
2025-06-20 23:32:08 +0300

    feat: add initial schema definition for files table

---
+++ apps/server/src/lib/db/schema.ts
@@ -0,0 +1,16 @@
+import { pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core';
+
+export const files = pgTable('files', {
+  id: uuid('id').defaultRandom().primaryKey(),
+
+  filename: text('filename').notNull(),
+  fileType: text('file_type').notNull(),
+  path: text('path').notNull(),
+  directory: text('directory').notNull(),
+
+  createdAt: timestamp('created_at', { withTimezone: true }).notNull(),
+  updatedAt: timestamp('updated_at', { withTimezone: true }).notNull(),
+  deletedAt: timestamp('deletedAt', { withTimezone: true }),
+
+  lastIndexedAt: timestamp('lastIndexedAt', { withTimezone: true }).notNull(),
+});
