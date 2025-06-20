5cc7a4c5d2c748c80e797bfb10dbd3418cb5ff8c
yedidya rashi <Yedidyarashi@gmail.com>
2025-06-20 20:11:54 +0300

    Add backend (#8)

---
+++ apps/server/src/main.ts
@@ -0,0 +1,30 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app/app.module';
+import z from 'zod';
+
+const envSchema = z.object({
+  PORT: z.coerce.number(),
+});
+
+export const env = envSchema.parse(process.env) as Readonly<
+  z.infer<typeof envSchema>
+>;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+  const port = env.PORT;
+  await app.listen(port);
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
+  );
+}
+
+bootstrap();
