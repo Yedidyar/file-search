612c62b2f48a0b302a54ec5434dc2cad4ae333fd
Nikita <nikitabecker2003@gmail.com>
2025-06-21 14:30:24 +0300

    feat: restructure database connection and schema setup; update TypeScript configurations

---
+++ apps/server/src/db/index.ts
@@ -0,0 +1,12 @@
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
+import * as schema from './schema';
+import { env } from '../env';
+
+
+
+const pool = new Pool({
+  connectionString: env.DATABASE_URL,
+});
+
+export const db = drizzle({ client: pool, schema: schema });
---
+++ apps/server/src/env.ts
@@ -0,0 +1,16 @@
+import { createEnv } from "@t3-oss/env-core";
+import { z } from 'zod';
+
+export const env = createEnv({
+  server: {
+    DATABASE_URL: z
+      .string({
+        required_error:
+          'DATABASE_URL environment variable is required for database connection',
+      })
+      .url(),
+    PORT: z.coerce.number(),
+  },
+
+  runtimeEnv: process.env,
+});
