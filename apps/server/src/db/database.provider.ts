c168b9b331e0a9a723428c508e50306eefbe7340
Nikita <nikitabecker2003@gmail.com>
2025-07-08 21:37:26 +0300

    refactor: remove mock database handling

--- apps/server/src/db/database.provider.ts
+++ apps/server/src/db/database.provider.ts
@@ -2,19 +2,10 @@ import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
 import * as schema from './schema';
 import { env } from '../env';
-import { mockDrizzle } from './mock-database.provider';
 
 const DATABASE_PROVIDER = 'DATABASE';
 
-const useMockDb = !!process.env.USE_MOCK_DB;
-
 const createDb = () => {
-  if (useMockDb) {
-    console.log('Using mock database for testing');
-    return mockDrizzle;
-  }
-
-  console.log('Connecting to real PostgreSQL database');
   const pool = new Pool({
     connectionString: env.DATABASE_URL,
   });
