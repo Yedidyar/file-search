98a420659750748061b5c23999de145d58a5728f
Nikita <nikitabecker2003@gmail.com>
2025-07-22 17:14:54 +0300

    tailwind v4

--- apps/client/postcss.config.js
+++ apps/client/postcss.config.js
@@ -1,10 +1,6 @@
-const { join } = require('path');
-
 module.exports = {
   plugins: {
-    '@tailwindcss/postcss': {
-      config: join(__dirname, 'tailwind.config.js'),
-    },
+    '@tailwindcss/postcss': {},
     autoprefixer: {},
   },
 };
