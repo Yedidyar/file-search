2f9ccef5d335e440c2bd640fc6aec202294784d4
Nikita <nikitabecker2003@gmail.com>
2025-07-22 17:44:47 +0300

    fix: better path management for client build

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -2,16 +2,22 @@ import { defineConfig } from 'vite';
 import { tanstackRouter } from '@tanstack/router-vite-plugin';
 import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import path from 'path';
 
 export default defineConfig({
+  root: __dirname,
   plugins: [
     nxViteTsPaths(),
     tanstackRouter({
-      routesDirectory: './frontend/routes',
-      generatedRouteTree: './frontend/routeTree.gen.ts',
+      routesDirectory: path.join(__dirname, 'frontend/routes'),
+      generatedRouteTree: path.join(__dirname, 'frontend/routeTree.gen.ts'),
     }),
     react(),
   ],
+  build: {
+    outDir: path.join(__dirname, '../../dist/apps/client'),
+    emptyOutDir: true,
+  },
   server: {
     port: 4200,
     host: 'localhost',
